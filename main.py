d596161d3e3e3e7979bd7c75f6ae213b9ca76f83
Mr_Katotohanan <jcapillanes_210000001122@uic.edu.ph>
2024-12-25 18:22:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 import connection
 
-
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
-    print(connection.con)
     return {"message": "Hello World"}
 
+@app.get("/clients")
+async def get_clients():
+    sqlQuery = "SELECT * FROM clients"
+
+    connection.my_cursor.execute(sqlQuery)
+    result = connection.my_cursor.fetchall()
+
+    return result
